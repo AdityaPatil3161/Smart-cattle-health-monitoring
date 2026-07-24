8cc8d37f938866ff7f39854bb8d7ae92ba61c495
Aditya Patil <patiladitya7604@gmail.com>
2026-07-24 23:28:23 +0530

    Create sensors.h

---
+++ sensors.h
@@ -0,0 +1,10 @@
+| Component       |  Quantity | Purpose                        |
+| --------------- | --------: | ------------------------------ |
+| ESP8266 NodeMCU |         1 | Main controller                |
+| MAX30102        |         1 | Heart rate & SpO₂              |
+| DS18B20         |         1 | Body temperature               |
+| DHT11           |         1 | Ambient temperature & humidity |
+| MPU6050         |         1 | Activity detection             |
+| NEO-6M GPS      |         1 | Location tracking              |
+| Breadboard      |         1 | Prototyping                    |
+| Jumper wires    | As needed | Connections                    |
